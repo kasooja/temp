576d7ccafa0fadf07ae2d9cb4c8694b1b9ddadcc
Kartik Asooja <asooja@gmail.com>
2017-02-06 10:25:16 +0000

    minor

--- src/main/java/parscit/XmlReaderTest.java
+++ src/main/java/parscit/XmlReaderTest.java
@@ -4,7 +4,7 @@ package parscit;
 import org.xml.sax.helpers.DefaultHandler;
 
 public class XmlReaderTest {	
-	public static String xml = "C:/Users/Kartik Asooja/Downloads/Parcit/A/A00/A00-1000-parscit.130908.xml";
+	public static String xml = "C:/Users/Kartik Asooja/Downloads/Parscit/A/A00/A00-1000-parscit.130908.xml";
 
 	public static void main(String[] args) {	
 		ParscitFile file = new ParscitFile();
